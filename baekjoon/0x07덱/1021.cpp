907b36e2abd5da941b1809c2c761c8bf237fb53a
i'mseojin <76787660+iamseojin@users.noreply.github.com>
2024-01-21 20:23:00 +0900

    Update 1021.cpp

--- baekjoon/0x07덱/1021.cpp
+++ baekjoon/0x07덱/1021.cpp
@@ -25,6 +25,14 @@ int main()
 			cnt++;
 		}
 		int r = (int)Dq.size() - cnt; 
+		/*
+		 지금은 cnt가 항상 DQ.size()보다 작아서 문제가 없지만
+		DQ.size()는 unsigned int(or unsigned long long)이기
+		때문에 만약 cnt가 DQ.size()보다 컸다면 overflow가 발생해
+		의도한대로 동작하지 않을 수 있음을 인지해야 함. 그래서 size()로
+		받아온 값에 대해서는 안전하게 (int)DQ.size() 로 항상 형변환을
+		하는 것도 괜찮음.
+		*/
 		Dq.pop_front();
 		ans += (cnt < r ? cnt : r);
 	}
