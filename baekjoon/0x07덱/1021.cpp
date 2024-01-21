39e53cdb0496919c6728dbb05105099966ab2ee5
i'mseojin <76787660+iamseojin@users.noreply.github.com>
2024-01-21 20:35:17 +0900

    Update 1021.cpp

--- baekjoon/0x07덱/1021.cpp
+++ baekjoon/0x07덱/1021.cpp
@@ -39,94 +39,138 @@ int main()
 	cout << ans;
 }
 
+// 바킹독님
+// Authored by : haneulkimdev
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/9571e70535a34702812d2a03510ac182
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    deque<int> DQ;
+    for (int i = 1; i <= n; i++) DQ.push_back(i);
+    int ans = 0;
+    while (m--) {
+        int t;
+        cin >> t;
+        int idx = find(DQ.begin(), DQ.end(), t) - DQ.begin(); // idx : t가 있는 위치
+        while (DQ.front() != t) {
+            if (idx < DQ.size() - idx) {
+                DQ.push_back(DQ.front());
+                DQ.pop_front();
+            }
+            else {
+                DQ.push_front(DQ.back());
+                DQ.pop_back();
+            }
+            ans++;
+        }
+        DQ.pop_front();
+    }
+    cout << ans;
+}
+
+/*
+20번째 줄에서, 지금은 idx가 항상 DQ.size()보다 작아서 문제가 없지만
+DQ.size()는 unsigned int(or unsigned long long)이기
+때문에 만약 idx가 DQ.size()보다 컸다면 overflow가 발생해
+의도한대로 동작하지 않을 수 있음을 인지해야 함. 그래서 size()로
+받아온 값에 대해서는 안전하게 (int)DQ.size() 로 항상 형변환을
+하는 것도 괜찮음.
+*/
 
 // 야매 배열로 구현 (시간은 빠르나, 공간복잡도가 너무 큼)
-//const int mx = 50;
-//int dq[2 * mx + 1];
-//int head = mx, tail = mx;
-//
-//void push_front(int x) {
-//	dq[--head] = x;
-//}
-//
-//void push_back(int x) {
-//	dq[tail++] = x;
-//}
-//
-//void pop_front() {
-//	head++;
-//}
-//
-//void pop_back() {
-//	tail--;
-//}
-//
-//int front() {
-//	return dq[head];
-//}
-//
-//int back() {
-//	return dq[tail - 1];
-//}
-//
-//int left(int x)
-//{
-//	int idx = head;
-//	int cnt = 0;
-//	while (dq[idx] != x)
-//	{
-//		idx++;
-//		cnt++;
-//	}
-//	return cnt;
-//}
-//
-//int right(int x)
-//{
-//	int idx = tail-1;
-//	int cnt = 1;
-//	while (dq[idx] != x)
-//	{
-//		idx--;
-//		cnt++;
-//	}
-//	return cnt;
-//}
-//
-//
-//int main()
-//{
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//	int n, k;
-//	cin >> n >> k;
-//	int x;
-//	int l, r;
-//	int cnt = 0;
-//	for (int i = 1; i <= n; i++) push_back(i);
-//	while (k--)
-//	{
-//		cin >> x;
-//		l = left(x); r = right(x);
-//		if (l < r)
-//		{
-//			cnt += l;
-//			while (l--)
-//			{
-//				push_back(front());
-//				pop_front();
-//			}
-//		}
-//		else
-//		{
-//			cnt += r;
-//			while (r--)
-//			{
-//				push_front(back());
-//				pop_back();
-//			}
-//		}
-//		pop_front();
-//	}
-//	cout << cnt;
-//}
+const int mx = 50;
+int dq[2 * mx + 1];
+int head = mx, tail = mx;
+
+void push_front(int x) {
+	dq[--head] = x;
+}
+
+void push_back(int x) {
+	dq[tail++] = x;
+}
+
+void pop_front() {
+	head++;
+}
+
+void pop_back() {
+	tail--;
+}
+
+int front() {
+	return dq[head];
+}
+
+int back() {
+	return dq[tail - 1];
+}
+
+int left(int x)
+{
+	int idx = head;
+	int cnt = 0;
+	while (dq[idx] != x)
+	{
+		idx++;
+		cnt++;
+	}
+	return cnt;
+}
+
+int right(int x)
+{
+	int idx = tail-1;
+	int cnt = 1;
+	while (dq[idx] != x)
+	{
+		idx--;
+		cnt++;
+	}
+	return cnt;
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, k;
+	cin >> n >> k;
+	int x;
+	int l, r;
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) push_back(i);
+	while (k--)
+	{
+		cin >> x;
+		l = left(x); r = right(x);
+		if (l < r)
+		{
+			cnt += l;
+			while (l--)
+			{
+				push_back(front());
+				pop_front();
+			}
+		}
+		else
+		{
+			cnt += r;
+			while (r--)
+			{
+				push_front(back());
+				pop_back();
+			}
+		}
+		pop_front();
+	}
+	cout << cnt;
+}
+
