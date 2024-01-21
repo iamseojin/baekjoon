a0dba0017587af51d8c04bce438aab3ae374836a
i'mseojin <76787660+iamseojin@users.noreply.github.com>
2024-01-21 20:20:07 +0900

    Update 1021.cpp

--- baekjoon/0x07덱/1021.cpp
+++ baekjoon/0x07덱/1021.cpp
@@ -1,3 +1,36 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <deque>
+using namespace std;
+#define fastio cin.tie(0); ios_base::sync_with_stdio(0);
+
+int N, K;
+
+int main()
+{
+	fastio;
+	deque<int> Dq;
+	cin >> N >> K;
+	int x;
+	int ans = 0;
+	for (int i = 1; i <= N; i++) Dq.push_back(i);
+	while (K--)
+	{
+		cin >> x;
+		int cnt = 0;
+		while (Dq.front() != x)
+		{
+			Dq.push_back(Dq.front());
+			Dq.pop_front();
+			cnt++;
+		}
+		int r = (int)Dq.size() - cnt; 
+		Dq.pop_front();
+		ans += (cnt < r ? cnt : r);
+	}
+	cout << ans;
+}
+
 
 // 야매 배열로 구현 (시간은 빠르나, 공간복잡도가 너무 큼)
 //const int mx = 50;
